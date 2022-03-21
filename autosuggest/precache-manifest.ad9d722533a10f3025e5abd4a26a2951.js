0ed237b71f73477e5ccba70915230e8986f47135
Maksim Makarov <maximmakarov1996@gmail.com>
2022-03-21 15:49:10 +0300

    Add files via upload

---
+++ autosuggest/precache-manifest.ad9d722533a10f3025e5abd4a26a2951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa4f924919d1a3cdd23af3da2ae9b87",
+    "url": "/autosuggest/index.html"
+  },
+  {
+    "revision": "70a3ea414cab80950c3d",
+    "url": "/autosuggest/static/css/main.0a413c71.chunk.css"
+  },
+  {
+    "revision": "38d734d59bcb56d95e02",
+    "url": "/autosuggest/static/js/2.21901f20.chunk.js"
+  },
+  {
+    "revision": "100e07e26dbc123e543852eb0db45bbb",
+    "url": "/autosuggest/static/js/2.21901f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a3ea414cab80950c3d",
+    "url": "/autosuggest/static/js/main.509ca452.chunk.js"
+  },
+  {
+    "revision": "798c9950a5472a8095c6",
+    "url": "/autosuggest/static/js/runtime-main.7fec9fbf.js"
+  }
+]);
